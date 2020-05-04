1fc5325e0408e6f824fec4e3469fe239afe8f5e7
Jonathan T Dean <jonathantomdean@gmail.com>
2020-05-04 21:53:49 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -13,6 +13,7 @@ def find_item_by_name_in_collection(name, collection)
    
 end
 
+# Check for Items
 def consolidate_cart(cart)
   updated_cart = Array.new 
 
@@ -27,21 +28,18 @@ def consolidate_cart(cart)
   end
 
   return item_counter(updated_cart, cart)
+  # return updated_cart
 end
 
+# Count the amount of items in the original cart
 def item_counter(new_cart, old_cart)
   new_cart.each do |element_newcart|
-    old_cart.each do |element_oldcart|
-      element_newcart.each do |updated_key, updated_value|
-        element_oldcart.each do |old_key, old_value|
-          if updated_key == :item && updated_value == old_value
-            element_newcart[:count] += 1
-          end
-        end
-      end
-    end
-  end
+		old_cart.each do |element_oldcart|
+			if element_newcart[:item] == element_oldcart[:item]
+				element_newcart[:count] += 1
+			end
+
+		end
+	end
 end
-
-
   
